c205274b99b3eb084cf97c888184675314177ff9
Thiago Hersan <thiago.hersan+github@gmail.com>
2019-02-07 11:57:32 +0000

    adds comments to run script

--- run_scan.sh
+++ run_scan.sh
@@ -2,6 +2,14 @@ gr-scan --sample-rate=16\
         --spread=250\
         --threshold=1\
         --fft-width=1024\
-        --start-frequency=10\
+        --start-frequency=2350\
+        --end-frequency=2550\
+        --output-csv=out_2350-2550_petal.csv
+
+echo "  --start-frequency=10\
         --end-frequency=1000\
-        --output-csv=out_10-1000.csv
+        "
+
+echo "  --start-frequency=2350\
+        --end-frequency=2550\
+        "
