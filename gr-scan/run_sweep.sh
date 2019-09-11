2211c742eeca0d5f035c82d08ca508554a9e676c
Thiago Hersan <thiago.hersan+github@gmail.com>
2019-09-10 22:41:10 -0300

    adds script for sweeping all freqs

---
+++ gr-scan/run_sweep.sh
@@ -0,0 +1,8 @@
+gr-scan --average=800\
+        --fine-bandwidth=25\
+        --threshold=0\
+        --start-frequency=50\
+        --end-frequency=2550\
+        --sample-rate=16\
+        --step=16\
+        --output-csv=out_800.csv
