85784a64178b1728a08aaa7e2d5f2a337ad7c105
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-02-10 07:52:21 +0100

    factors out init code in tx-rx.c so we can open and close new connections to hackrfs within frequency loop

--- cpp/tx-rx.c
+++ cpp/tx-rx.c
@@ -2,33 +2,46 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <fftw3.h>
 
 static hackrf_device* tx_device = NULL;
 static hackrf_device* rx_device = NULL;
 
 unsigned int MIN_FREQ_MHZ = 50;
-// int MAX_FREQ_MHZ = 2550;
-unsigned int MAX_FREQ_MHZ = 150;
+unsigned int MAX_FREQ_MHZ = 2550;
 unsigned int SAMPLE_RATE_MHZ = 20;
 
 // Last HackRf to be plugged in is id=0
 int RX_ID = 0;
 
-uint64_t samples_to_rxfer = 2e6;
+uint32_t lna_gain = 16;
+uint32_t vga_gain = 22;
+uint32_t txvga_gain = 42;
+uint32_t sample_rate_hz = SAMPLE_RATE_MHZ * 1e6;
+
+uint64_t samples_to_rxfer = 1 << 22; // ~ 4e6
 uint64_t bytes_to_rxfer = 2 * samples_to_rxfer;
 int8_t* rxsamples;
+int8_t rxsamples_max;
+int8_t rxsamples_min;
+
+//fftwf_complex* fftwIn = NULL;
+//fftwf_complex* fftwOut = NULL;
+//fftwf_plan fftwPlan = NULL;
 
 int tx_callback(hackrf_transfer* transfer) {
     for(unsigned int i = 0; i < transfer->buffer_length; i++)
-        transfer->buffer[i] = (uint8_t)(rand() & 0xff);
+        transfer->buffer[i] = 0x7f;
     return 0;
 }
 
 int rx_callback(hackrf_transfer* transfer) {
-	int bytes_to_read = transfer->valid_length;
+    int bytes_to_read = transfer->valid_length;
+    int8_t* tbuf = (int8_t*) transfer->buffer;
 
     if (bytes_to_read >= bytes_to_rxfer) {
         bytes_to_read = bytes_to_rxfer;
@@ -36,11 +49,34 @@ int rx_callback(hackrf_transfer* transfer) {
 
     int offset = 2 * samples_to_rxfer - bytes_to_rxfer;
 
-    for(unsigned int i = 0; i < bytes_to_read; i++)
-        rxsamples[offset + i] = transfer->buffer[i];
+    /**/
+    for(unsigned int i = 0; i < bytes_to_read; i++) {
+        rxsamples[offset + i] = tbuf[i];
+        if(tbuf[i] > rxsamples_max) {
+            rxsamples_max = tbuf[i];
+        }
+        if(tbuf[i] < rxsamples_min) {
+            rxsamples_min = tbuf[i];
+        }
+    }
+    /**/
+
+    /*
+    for(unsigned int i = 0; i < bytes_to_read; i += 2) {
+        fftwIn[(offset + i) / 2][0] = tbuf[i + 0]  / 128.0f;
+        fftwIn[(offset + i) / 2][1] = tbuf[i + 1] / 128.0f;
+    }
+    /**/
 
     bytes_to_rxfer -= bytes_to_read;
-    return -1 * (bytes_to_rxfer == 0);
+    return 0;
+}
+
+float logPower(fftwf_complex in, float scale) {
+	float re = in[0] * scale;
+	float im = in[1] * scale;
+	float magsq = re * re + im * im;
+	return (float) (log2(magsq) * 10.0f / log2(10.0f));
 }
 
 void millisleep(int mls) {
@@ -51,141 +87,155 @@ void millisleep(int mls) {
     return;
 }
 
+void cleanUpExit() {
+    free(rxsamples);
+//    fftwf_free(fftwIn);
+//    fftwf_free(fftwOut);
+    hackrf_exit();
+}
+
 int check(int result, const char msg[]) {
     if( result != HACKRF_SUCCESS ) {
         fprintf(stderr, "%s failed: %s (%d)\n",
                 msg,
                 hackrf_error_name((hackrf_error)result),
                 result);
-        free(rxsamples);
-        hackrf_exit();
+        cleanUpExit();
         return HACKRF_ERROR_OTHER;
     } else {
         return HACKRF_SUCCESS;
     }
 }
 
+int initRx(uint32_t freq) {
+    return hackrf_set_sample_rate(rx_device, sample_rate_hz) |
+        hackrf_set_hw_sync_mode(rx_device, 0) |
+        hackrf_set_amp_enable(rx_device, 1) |
+        hackrf_set_antenna_enable(rx_device, 0) |
+        hackrf_set_lna_gain(rx_device, lna_gain) |
+        hackrf_set_vga_gain(rx_device, vga_gain) |
+        hackrf_set_freq(rx_device, freq);
+}
+
+int initTx(uint32_t freq) {
+    return hackrf_set_sample_rate(tx_device, sample_rate_hz) |
+        hackrf_set_hw_sync_mode(tx_device, 0) |
+        hackrf_set_amp_enable(tx_device, 1) |
+        hackrf_set_antenna_enable(tx_device, 0) |
+        hackrf_set_txvga_gain(tx_device, txvga_gain) |
+        hackrf_set_freq(tx_device, freq);
+}
+
 int main(int argc, char** argv) {
-	unsigned int lna_gain=16, vga_gain=22, txvga_gain=42;
-    uint32_t sample_rate_hz = SAMPLE_RATE_MHZ * 1e6;
     uint32_t txrx_freq_hz = MIN_FREQ_MHZ * 1e6;
 
-    rxsamples = (int8_t*) calloc(bytes_to_rxfer, sizeof(int8_t));
-
+    char outfilename_root[128];
     char outfilename[128];
-    strcpy(outfilename, "out/out_");
+    strcpy(outfilename_root, "out/out_");
 
     fprintf(stdout, "Enter a name/nickname: ");
-    scanf("%[^\n]%*c", &outfilename[8]);
-    strcat(outfilename, ".csv");
+    scanf("%[^\n]%*c", &outfilename_root[8]);
+    strncpy(outfilename, outfilename_root, 128);
+
+    rxsamples = (int8_t*) calloc(bytes_to_rxfer, sizeof(int8_t));
+//    fftwIn = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * samples_to_rxfer);
+//    fftwOut = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * samples_to_rxfer);
+//    fftwPlan = fftwf_plan_dft_1d(samples_to_rxfer, fftwIn, fftwOut, FFTW_FORWARD, FFTW_ESTIMATE);
 
     /////// init hackrf env
     if(check(hackrf_init(), "hackrf_init")) {
         return EXIT_FAILURE;
     }
 
-    /////// init 2 hackrfs
     hackrf_device_list_t* device_list = hackrf_device_list();
 
-    if(check(hackrf_device_list_open(device_list, (1 - RX_ID), &tx_device), "tx open")) {
-        return EXIT_FAILURE;
-    }
-
-    if(check(hackrf_device_list_open(device_list, RX_ID, &rx_device), "rx open")) {
-        return EXIT_FAILURE;
-    }
-
-    /////// set sample rates
-    if(check(hackrf_set_sample_rate(tx_device, sample_rate_hz), "tx sample rate")) {
-        return EXIT_FAILURE;
-    }
-    if(check(hackrf_set_sample_rate(rx_device, sample_rate_hz), "rx sample rate")) {
-        return EXIT_FAILURE;
-    }
-
-    ///// set tx gains
-    if(check(hackrf_set_amp_enable(tx_device, 1), "tx amp")) {
-        return EXIT_FAILURE;
-    }
-    if(check(hackrf_set_txvga_gain(tx_device, txvga_gain), "tx gains")) {
-        return EXIT_FAILURE;
-    }
-
-    ///// set rx gains
-    if(check(hackrf_set_amp_enable(rx_device, 1), "rx amp")) {
-        return EXIT_FAILURE;
-    }
-    if(check(hackrf_set_vga_gain(rx_device, vga_gain) |
-             hackrf_set_lna_gain(rx_device, lna_gain), "rx gains")) {
-        return EXIT_FAILURE;
-    }
-
-
     ////// start and stop tx many times
-    for(unsigned int rxf = MIN_FREQ_MHZ; rxf <= MAX_FREQ_MHZ; rxf += SAMPLE_RATE_MHZ) {
+    for(uint16_t rxf = MIN_FREQ_MHZ; rxf <= MAX_FREQ_MHZ; rxf += SAMPLE_RATE_MHZ) {
         fprintf(stdout, "txing %d MHz\n", rxf);
         txrx_freq_hz = rxf * 1e6;
 
-        // start tx
-        if(check(hackrf_set_freq(tx_device, txrx_freq_hz) |
-                 hackrf_start_tx(tx_device, tx_callback, NULL), "tx start")) {
+        // TX
+        if(check(hackrf_device_list_open(device_list, (1 - RX_ID), &tx_device), "tx open")) {
             return EXIT_FAILURE;
         }
+        if(check(initTx(txrx_freq_hz), "init Tx")) {
+            return EXIT_FAILURE;
+        }
+        if(check(hackrf_start_tx(tx_device, tx_callback, NULL), "tx start")) {
+            return EXIT_FAILURE;
+        }
+        millisleep(100);
 
-
-        // start rx
+        // RX
         bytes_to_rxfer = 2 * samples_to_rxfer;
-        if(check(hackrf_set_freq(rx_device, txrx_freq_hz) |
-                 hackrf_start_rx(rx_device, rx_callback, NULL), "rx start")) {
+        rxsamples_max = 0;
+        rxsamples_min = 0;
+
+        if(check(hackrf_device_list_open(device_list, RX_ID, &rx_device), "rx open")) {
+            return EXIT_FAILURE;
+        }
+        if(check(initRx(txrx_freq_hz), "init Rx")) {
+            return EXIT_FAILURE;
+        }
+        if(check(hackrf_start_rx(rx_device, rx_callback, NULL), "rx start")) {
             return EXIT_FAILURE;
         }
 
         while(bytes_to_rxfer > 0) {
             millisleep(10);
         }
-        fprintf(stdout, "done rxing!! \n");
+        printf("minmax: %d %d\n", rxsamples_min, rxsamples_max);
 
         // stop rx and tx
-        if(check(hackrf_stop_rx(rx_device), "rx stop")) {
-            return EXIT_FAILURE;
-        }
         if(check(hackrf_stop_tx(tx_device), "tx stop")) {
             return EXIT_FAILURE;
         }
+        if(check(hackrf_close(tx_device), "tx close")) {
+            return EXIT_FAILURE;
+        }
 
-        // have samples in rxsamples
-        // TODO: fft or output
+        if(check(hackrf_stop_rx(rx_device), "rx stop")) {
+            return EXIT_FAILURE;
+        }
+        if(check(hackrf_close(rx_device), "rx close")) {
+            return EXIT_FAILURE;
+        }
+        millisleep(100);
+
+        // fftwf_execute(fftwPlan);
 
         /*
-        mean = np.mean(samples)
-        samples = samples - mean;
+        strncpy(outfilename, outfilename_root, 128);
 
-        Ps, fs = psd(samples, NFFT=FFT_SIZE, Fs=rx.sample_rate/1e6, Fc=rx.center_freq/1e6)
+        char intbuf[9];
+        snprintf(intbuf, 9, "_%03d.csv", rxf);
+        strcat(outfilename, intbuf);
 
-        for i in range(len(Ps)):
-            if (fs[i] % 1.0 == 0.0):
-                freq_to_power[fs[i]] = np.log(Ps[i])
+        FILE *outfile = fopen(outfilename, "wb");
+        fwrite(rxsamples, sizeof(int8_t), 2 * samples_to_rxfer, outfile);
 
-        with open(os.path.join('out', outfilename), 'a') as out:
-            for freq in freq_to_power:
-                out.write('%s,%s\n' % (freq, freq_to_power[freq]))
-        */
-        FILE *outfile = fopen(outfilename,"a");
-        fprintf(outfile, "%d,%d\n", 12, 12345);
+        float fftstep = SAMPLE_RATE_MHZ * 1e6 / samples_to_rxfer;
+        for (unsigned int i = 1; i < samples_to_rxfer / 2; i++) {
+            float mpower = logPower(fftwOut[i], 1.0f / samples_to_rxfer);
+            float freq = (txrx_freq_hz + (i * fftstep)) / 1e6;
+            if(fmod(freq, 1.0) == 0.0)
+                fprintf(outfile, "%f,%f\n", freq, mpower);
+		}
+
         fclose(outfile);
-
-        millisleep(100);
+        */
     }
 
-    //// close devices
-    if(check(hackrf_close(tx_device), "tx close")) {
-        return EXIT_FAILURE;
-    }
-    if(check(hackrf_close(rx_device), "rx close")) {
-        return EXIT_FAILURE;
-    }
+    /*
+    fprintf(stdout, "first\n");
+    fprintf(stdout, "%d %d\n", rxsamples[0], rxsamples[1]);
+    fprintf(stdout, "%f %f\n", fftwIn[0][0], fftwIn[0][1]);
 
-    hackrf_exit();
-    free(rxsamples);
+    fprintf(stdout, "last\n");
+    fprintf(stdout, "%d %d\n", rxsamples[2 * samples_to_rxfer - 2], rxsamples[2 * samples_to_rxfer - 1]);
+    fprintf(stdout, "%f %f\n", fftwIn[samples_to_rxfer - 1][0], fftwIn[samples_to_rxfer - 1][1]);
+    /**/
+
+    cleanUpExit();
     return EXIT_SUCCESS;
 }
